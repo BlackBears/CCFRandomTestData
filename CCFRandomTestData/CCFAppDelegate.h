ba68ce59fdd655f509dadc3e269eb1408188a4ae
Alan K. Duncan <alan@cocoa-factory.com>
2013-08-16 13:28:02 -0500

    Initial Commit

---
+++ CCFRandomTestData/CCFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCFAppDelegate.h
+//  CCFRandomTestData
+//
+//  Created by alanduncan on 8/16/13.
+//  Copyright (c) 2013 Cocoa Factory, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CCFRandomTestData/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CCFRandomTestData
+//
+//  Created by alanduncan on 8/16/13.
+//  Copyright (c) 2013 Cocoa Factory, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CCFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CCFAppDelegate class]));
+    }
+}
